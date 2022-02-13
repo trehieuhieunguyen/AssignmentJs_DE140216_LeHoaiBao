8934e7c71852a73ae75c1b1e0703d145842f34cb
hieundde140223 <59632468+hieundde140223@users.noreply.github.com>
2022-02-13 18:01:06 +0700

    hieu
    ds

---
+++ 11/11.js
@@ -0,0 +1,12 @@
+document.getElementById("gioithieu").addEventListener("click",function(){
+    window.open('https://gokisoft.com', '_blank', 'toolbar=0,location=0,menubar=0');
+});
+document.getElementById("tintuc").addEventListener("click",function(){
+    window.open('https://ziczacgroup.com', '_blank', 'toolbar=0,location=0,menubar=0');
+});
+document.getElementById("sanpham").addEventListener("click",function(){
+    window.open('https://thietkewebsitenhe.vn', '_blank', 'toolbar=0,location=0,menubar=0');
+});
+document.getElementById("trogiup").addEventListener("click",function(){
+    window.open('https://google.com', '_blank', 'toolbar=0,location=0,menubar=0');
+});
